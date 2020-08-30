65d394de9255702ee76b1d101d232e6d4e59046b
Steve <heystevegray@gmail.com>
2020-08-30 01:58:02 -0400

    Updated tests.

--- src/tests/useOnlineStatus.test.ts
+++ src/tests/useOnlineStatus.test.ts
@@ -1,12 +1,12 @@
 import { renderHook } from "@testing-library/react-hooks";
 import useOnlineStatus from "../hooks/use-online-status";
 
-test("should useOnlineStatus", () => {
+test("useOnlineStatus should match navigator.online", () => {
   const { result } = renderHook(() => useOnlineStatus());
   expect(result.current.online).toBe(navigator.onLine);
 });
 
 test("useOnlineStatus should be defined", () => {
   const { result } = renderHook(() => useOnlineStatus());
-  expect(result.current.online).toBe(navigator.onLine);
+  expect(result.current.online).toBeDefined();
 });

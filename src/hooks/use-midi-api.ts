08771d6fbb5c7e5b5f8ecca6393eb12ab351c284
Steve <heystevegray@gmail.com>
2020-09-07 00:13:04 -0400

    Midi connection event and midi port.

--- src/hooks/use-midi-api.ts
+++ src/hooks/use-midi-api.ts
@@ -1,43 +1,125 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import "web-midi-api";
 
 // 🙃😝 haha
-interface MIDInterface {
+export interface MIDInterface {
   midiAccess: WebMidi.MIDIAccess | undefined;
+  midiConnectionEvent: WebMidi.MIDIConnectionEvent | undefined;
   midiSupported: boolean | undefined;
+  midiPort?: WebMidi.MIDIPort | undefined;
+  midi: MidiNote;
 }
 
-const getMidiAccess = async (): Promise<WebMidi.MIDIAccess> => {
-  return await navigator.requestMIDIAccess();
-};
+interface Key {
+  value: number;
+  name: string;
+  octave: number;
+}
 
-const getMidiSupport = async (): Promise<boolean> => {
-  const access = await getMidiAccess();
-  return access ? true : false;
+interface MidiNote extends Key {
+  velocity: number;
+  on: boolean;
+}
+
+const noteName: string[] = [
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B",
+];
+
+const initialNote: MidiNote = {
+  value: 0,
+  octave: 0,
+  name: "N/a",
+  velocity: 0,
+  on: false,
 };
 
 const useMidiApi = (): MIDInterface => {
-  const [midi, setMidi] = useState<MIDInterface>({
+  const [midiConfig, setMidiConfig] = useState<MIDInterface>({
     midiSupported: undefined,
     midiAccess: undefined,
+    midiConnectionEvent: undefined,
+    midi: initialNote,
   });
+  const midiConfigRef = useRef(midiConfig);
 
-  //   useEffect(() => {
-  //     // console.log("Internal", { midi });
-  //   }, [midi]);
+  const update = (data: Partial<MIDInterface>): void => {
+    const merged = { ...midiConfigRef.current, ...data };
+    midiConfigRef.current = merged;
+    setMidiConfig(merged);
+  };
+
+  const getNoteName = (midiNote: number): Key => {
+    const octave: number = Math.floor(midiNote / 12 - 1);
+    const noteIndex: number = midiNote % 12;
+    const name = noteName[noteIndex];
+    return { value: midiNote, octave, name };
+  };
+
+  const onMIDISuccess = (midiAccess: any): void => {
+    // Subscribe to Midi messages
+    for (var input of midiAccess.inputs.values()) {
+      input.onmidimessage = getMIDIMessage;
+    }
+    // Subscribe to Midi state changes
+    midiAccess.onstatechange = getStateChange;
+    update({ midiSupported: true, midiAccess });
+  };
+
+  const onMIDIFailure = (): void => {
+    console.log("Could not access your MIDI devices.");
+    update({ midiSupported: false });
+  };
+
+  const getStateChange = (message: WebMidi.MIDIConnectionEvent): void => {
+    update({ midiConnectionEvent: message, midiPort: message.port });
+  };
+
+  const getMIDIMessage = (message: WebMidi.MIDIMessageEvent): void => {
+    var command = message.data[0];
+    var note = message.data[1];
+    // a velocity value might not be included with a noteOff command
+    var velocity = message.data.length > 2 ? message.data[2] : 0;
+
+    switch (command) {
+      case 144: // note on
+        if (velocity > 0) {
+          update({ midi: { ...getNoteName(note), velocity, on: true } });
+        } else {
+          update({ midi: { ...getNoteName(note), velocity, on: false } });
+        }
+        break;
+      case 128: // note off
+        update({ midi: { ...getNoteName(note), velocity, on: false } });
+        break;
+    }
+  };
 
   useEffect(() => {
-    console.log("use-midi-api!");
     const initialize = async (): Promise<any> => {
-      setMidi({
-        midiSupported: await getMidiSupport(),
-        midiAccess: await getMidiAccess(),
-      });
+      try {
+        navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+      } catch (error) {
+        console.log({ error });
+        update({ midiSupported: false });
+      }
     };
 
     initialize();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return midi;
+  return midiConfig;
 };
 
 export default useMidiApi;
